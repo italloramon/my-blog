bd10d9fbf36a763bfff8ce22a2a66148f41aba9b
Itallo Ramon <irvp@ic.ufal.br>
2024-07-27 19:07:28 -0300

    feat: add routing to contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
